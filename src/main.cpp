03fe29090f79584da627b5451af327a2725843ad
Thomas HOULLIER <pro@houllier.net>
2024-02-11 19:18:00 +0100

    Showing how to create a GTK window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "version.h"
+#include <gtkmm.h>
 
-int main () {
+class MyWindow : public Gtk::Window
+{
+public:
+  MyWindow();
+};
+
+MyWindow::MyWindow()
+{
+  set_title("Basic application");
+}
+
+int main (int argc, char* argv[]) {
   std::cout << "# timesheeting main #" << std::endl;
+  std::cout << "Version: " << TIMESHEETING_VERSION << std::endl;
+
+  auto app = Gtk::Application::create("org.gtkmm.examples.base");
+  return app->make_window_and_run<MyWindow>(argc, argv);
 }
---
+++ src/version.h
@@ -0,0 +1,6 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+const char* const TIMESHEETING_VERSION = "0.0";
+
+#endif // VERSION_H
