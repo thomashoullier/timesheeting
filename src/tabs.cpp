b2efcdefe1e8f82aec5733bf63e7ec5a8c6ce869
Thomas HOULLIER <pro@houllier.net>
2024-02-18 10:24:30 +0100

    Named all required tabs.

--- src/tabs.cpp
+++ src/tabs.cpp
@@ -1,10 +1,16 @@
 #include "tabs.h"
 
 Tabs::Tabs () {
-  auto m_Label1 = Gtk::make_managed<Gtk::Label>("Contents of tab 1");
-  auto m_Label2 = Gtk::make_managed<Gtk::Label>("Contents of tab 2");
+  auto label_daily = Gtk::make_managed<Gtk::Label>("Daily entries");
+  auto label_hierarchy = Gtk::make_managed<Gtk::Label>("Hierarchy items");
+  auto label_projects = Gtk::make_managed<Gtk::Label>("Project totals");
+  auto label_weekly = Gtk::make_managed<Gtk::Label>("Weekly report");
+  auto label_export = Gtk::make_managed<Gtk::Label>("Export");
 
-  this->append_page(*m_Label1, "First");
-  this->append_page(*m_Label2, "Second");
+  this->append_page(*label_daily, "Daily entries");
+  this->append_page(*label_hierarchy, "Hierarchy items");
+  this->append_page(*label_projects, "Project total");
+  this->append_page(*label_weekly, "Weekly report");
+  this->append_page(*label_export, "Export");
   this->set_expand(true);
 }
