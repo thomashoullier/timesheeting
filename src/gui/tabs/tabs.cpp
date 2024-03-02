f59137ce34b7f0ecca4953bd57723cdad34d3390
Thomas HOULLIER <pro@houllier.net>
2024-03-02 11:33:38 +0100

    Drafted a treeview for entries.

--- src/gui/tabs/tabs.cpp
+++ src/gui/tabs/tabs.cpp
@@ -3,9 +3,13 @@
 #include "gtkmm/box.h"   // TODO
 #include "gtkmm/centerbox.h"                         //
 #include "gtkmm/calendar.h" // TODO
+#include "gtkmm/liststore.h"
 #include "gtkmm/object.h"
 #include "gtkmm/button.h"
 #include "glibmm/date.h"
+#include "gtkmm/treemodelcolumn.h"
+#include "gtkmm/treeview.h"
+#include "giomm/liststore.h"
 
 Tabs::Tabs() {
   auto label_daily = Gtk::make_managed<Gtk::Label>("Daily entries");
@@ -25,20 +29,32 @@ Tabs::Tabs() {
   next_day->set_icon_name("go-next");
   // Day display + cal dialog opening
   auto cal_day = Gtk::make_managed<Gtk::Button>();
-  auto day_str =
-    Glib::Date(Glib::Date::Day(2),Glib::Date::Month(3),Glib::Date::Year(2024));
+  auto day_str = Glib::Date(Glib::Date::Day(2), Glib::Date::Month(3),
+                            Glib::Date::Year(2024));
   cal_day->set_label(day_str.format_string("%a, %d %b %Y"));
 
   cal_hbox->set_start_widget(*prev_day);
   cal_hbox->set_center_widget(*cal_day);
   cal_hbox->set_end_widget(*next_day);
 
-  auto cal = Gtk::make_managed<Gtk::Calendar>();
+  // TreeView of entries
+  auto treev = Gtk::make_managed<Gtk::TreeView>();
+  auto m_cols = Gtk::TreeModelColumnRecord();
+  auto col1 = Gtk::TreeModelColumn<int>();
+  auto col2 = Gtk::TreeModelColumn<int>();
+  m_cols.add(col1);
+  m_cols.add(col2);
+  auto m_list = Gtk::ListStore::create(m_cols);
+  treev->set_model(m_list);
+
+  treev->append_column("YO", col1);
+  treev->append_column("HA", col2);
+  m_list->append();
+
   daily_vbox->append(*cal_hbox);
+  daily_vbox->append(*treev);
 
   this->append_page(*daily_vbox, "Daily entries");
-
-
   this->append_page(*label_hierarchy, "Hierarchy items");
   this->append_page(*label_projects, "Project total");
   this->append_page(*label_weekly, "Weekly report");
