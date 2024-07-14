55476e963a088237a2f82a814d3fc61696011149
Thomas HOULLIER <pro@houllier.net>
2024-07-14 15:24:38 +0200

    Scrapped the whole source code, we are not doing a gtk software after all.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
 
-#include "application.h"
-#include "globals.h"
-
-int main() {
-  std::cout << "# timesheeting main #" << std::endl;
-  std::cout << "Version: " << TIMESHEETING_VERSION << std::endl;
-
-  Application app;
-  return app.run_gui();
+int main () {
+  std::cout << "Hello" << std::endl;
 }
