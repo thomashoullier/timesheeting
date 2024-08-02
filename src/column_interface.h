93c9229febbe575b0156567b7d89d520a64104a7
Thomas HOULLIER <pro@houllier.net>
2024-08-02 20:51:47 +0200

    Initiated the Doxyfile, documented the column interface.

--- src/column_interface.h
+++ src/column_interface.h
@@ -1,3 +1,5 @@
+/** @file
+ * @brief Generic GUI column interface and utilities. */
 #ifndef COLUMN_INTERFACE_H
 #define COLUMN_INTERFACE_H
 
@@ -5,38 +7,38 @@
 #include <exception>
 #include <vector>
 
-/** Column position on the screen. */
+/** @brief Column position on the screen. */
 enum ColPos { left = 0, middle = 1, right = 2 };
 
-/** Interface for a column holding items. Part which does not
-    depend on the item type. */
+/** @brief Interface for a column holding items. Part which does not depend on
+    the item type. */
 class ColumnInterfaceBase {
 public:
   virtual ~ColumnInterfaceBase () = default;
-  /** Get the Id for the currently selected item. */
+  /** @brief Get the Id for the currently selected item. */
   virtual Id get_current_id() = 0;
-  /** Get the name for the currently selected item
-      and empty string if no items are selected. */
+  /** @brief Get the name for the currently selected item and empty string if no
+      items are selected. */
   virtual std::string get_current_name() = 0;
-  /** Select the next item. */
+  /** @brief Select the next item. */
   virtual void select_next_item() = 0;
-  /** Select the previous item. */
+  /** @brief Select the previous item. */
   virtual void select_prev_item() = 0;
-  /** Query a user input character and pass it. */
+  /** @brief Query a user input character and pass it. */
   virtual char query_input() = 0;
-  /** Query a name for a new item. Empty on cancellation. */
+  /** @brief Query a name for a new item. Empty on cancellation. */
   virtual std::string query_new_item_name() = 0;
-  /** Query a new name for the current item. */
+  /** @brief Query a new name for the current item. */
   virtual std::string query_current_item_rename () = 0;
-  /** Refresh the display. */
+  /** @brief Refresh the display. */
   virtual void refresh () = 0;
 
 protected:
-  /** Definition of the intended column position. */
+  /** @brief Definition of the intended column position. */
   ColPos pos;
 };
 
-/** Interface for a column holding a set of GenericItem, navigating
+/** @brief Interface for a column holding a set of GenericItem, navigating
     and querying user inputs. */
 template <typename T,
           typename = std::enable_if_t<std::is_base_of<GenericItem, T>::value>>
@@ -46,15 +48,16 @@ public:
   // TODO: no virtual constructor, how do we constrain the constructor?
   // /** Create a column at the given position. */
   // ColumnInterface(const std::vector<T> &items, ColPos pos) = 0;
-  /** Set or refresh the current list of items held by the column. */
+  /** @brief Set or refresh the current list of items held by the column. */
   virtual void set_items(const std::vector<T> &items) = 0;
 
 };
 
-/** Exception when encountering an empty column when it is required to
-    contain items for good operation. */
+/** @brief Exception when encountering an empty column when running an operation
+    which requires items. */
 class ColumnEmpty : public std::exception {
 private:
+  /** @brief Exception message. */
   std::string msg;
 
 public:
