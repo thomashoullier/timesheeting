d09380a6e7b4183f7584c0ab03a9b72060419a36
Thomas HOULLIER <pro@houllier.net>
2024-09-14 13:40:06 +0200

    Removed useless abstractions around logger.

--- src/logger/logger.cpp
+++ src/logger/logger.cpp
@@ -1,17 +1,68 @@
 #include "logger.h"
-#include "logger_file.h"
+#include <chrono>
+#include <ratio>
+#include <sstream>
+#include <string>
+
+Logger& Logger::get() {
+  static Logger instance ("timesheeting.log");
+  return instance;
+}
+
+void Logger::log(const std::string &message) {
+  if (!file->good()) {
+    throw std::runtime_error("Log file cannot be written to.");
+  }
+  *file << get_timestamp() << " : " << message << std::endl;
+}
+
+void Logger::tick() {
+  if (counting) {
+    return;
+  } else {
+    counting = true;
+    tick_point = std::chrono::high_resolution_clock::now();
+  }
+}
+
+void Logger::tock() {
+  auto tock_point = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double, std::milli> dur = tock_point - tick_point;
+  std::ostringstream ostr;
+  ostr.precision(3);
+  ostr << std::fixed << dur.count();
+  this->log("Input loop was: " + ostr.str() + " ms.");
+  counting = false;
+}
+
+Logger::Logger(const std::filesystem::path &log_file)
+    : file(std::make_unique<std::ofstream>(log_file, std::ios::app)),
+      counting(false) {
+  if (!file->good()) {
+    throw std::runtime_error("Log file was not opened correctly.");
+  }
+}
+
+std::string Logger::get_timestamp() {
+  auto date = std::chrono::system_clock::now();
+  auto date_t = std::chrono::system_clock::to_time_t(date);
+  char timeString[std::size("yyyy-mm-dd hh:mm:ss")];
+  std::strftime(std::data(timeString), std::size(timeString),
+                "%F %T", std::localtime(&date_t));
+  return timeString;
+}
 
 void log(const std::string &msg) {
-  auto *logger = &LoggerFile::get();
+  auto *logger = &Logger::get();
   logger->log(msg);
 }
 
 void tick() {
-  auto *logger = &LoggerFile::get();
+  auto *logger = &Logger::get();
   logger->tick();
 }
 
 void tock() {
-  auto *logger = &LoggerFile::get();
+  auto *logger = &Logger::get();
   logger->tock();
 }
--- src/logger/logger.h
+++ src/logger/logger.h
@@ -1,17 +1,43 @@
 /** @file
- * @brief Logger call functions.
- *
- * This is the place to define which LoggerInterface implementation is used. */
+ * @brief Logger interface implementation with a log file. */
 #ifndef LOGGER_H
 #define LOGGER_H
 
-#include <string>
+#include <filesystem>
+#include <memory>
+#include <fstream>
+#include <chrono>
 
-/** @brief Call an implementation of LoggerInterface::log(). */
+/** @brief Logger implementation with a log file.
+ *
+ * This is a singleton. */
+class Logger {
+public:
+  /** @brief Grab the log file singleton. */
+  static Logger& get();
+  void log (const std::string &message);
+  void tick ();
+  void tock ();
+
+private:
+  /** @brief Handle to the log file for output. */
+  std::unique_ptr<std::ofstream> file;
+  /** @brief Start time point for clocking durations. */
+  std::chrono::high_resolution_clock::time_point tick_point;
+  /** @brief Clock state. */
+  bool counting;
+
+  /** @brief Logger constructor using a file. */
+  Logger (const std::filesystem::path &log_file_path);
+  /** @brief Get the current date as a timestamp. */
+  std::string get_timestamp ();
+};
+
+/** @brief Grab the logger and call log(). */
 void log(const std::string &msg);
-/** @brief Call an implementation of LoggerInterface::tick(). */
+/** @brief Grab the logger and call tick(). */
 void tick();
-/** @brief Call an implementation of LoggerInterface::tock(). */
+/** @brief Grab the logger and call tock(). */
 void tock();
 
 #endif // LOGGER_H
