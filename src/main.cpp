8b6ea1f18db8fdb0220221341d01e73e74d24d9a
Thomas HOULLIER <pro@houllier.net>
2024-11-23 12:02:01 +0100

    All std::exception are now caught in main and their message printed.

--- src/main.cpp
+++ src/main.cpp
@@ -8,47 +8,54 @@
 #include "ui/tui/ui.h"
 #include "config/config.h"
 #include "version/version.h"
+#include <exception>
+#include <iostream>
 
 int main(int argc, const char *const *argv) {
-  // CLI
-  auto cli_data = cli::parse(argc, argv);
+  try {
+    // CLI
+    auto cli_data = cli::parse(argc, argv);
 
-  // Loading the configuration file.
-  auto config = (cli_data.config_filepath.empty()) ?
-    config::ConfigLoader().load() :
-    config::ConfigLoader().load(cli_data.config_filepath);
+    // Loading the configuration file.
+    auto config = (cli_data.config_filepath.empty()) ?
+      config::ConfigLoader().load() :
+      config::ConfigLoader().load(cli_data.config_filepath);
 
-  // Initialize the TimeZone.
-  time_lib::TimeZone::get(config.timezone);
+    // Initialize the TimeZone.
+    time_lib::TimeZone::get(config.timezone);
 
-  // Initialize the Logger.
-  log_lib::Logger::get(config.log_filepath, config.log_levels_to_activate);
-  log_lib::logger().tick(); // For measuring startup time.
+    // Initialize the Logger.
+    log_lib::Logger::get(config.log_filepath, config.log_levels_to_activate);
+    log_lib::logger().tick(); // For measuring startup time.
 
-  log_lib::Logger::get().
-    log("Launching timesheeting v" + version::TIMESHEETING_VERSION,
-        log_lib::LogLevel::info);
+    log_lib::Logger::get().
+      log("Launching timesheeting v" + version::TIMESHEETING_VERSION,
+          log_lib::LogLevel::info);
 
-  // Initialize the DB.
-  db::DB_SQLite::get(config.db_filepath);
+    // Initialize the DB.
+    db::DB_SQLite::get(config.db_filepath);
 
-  // Initialize the key bindings.
-  keys::BoundKeys::get(&config.bindings);
+    // Initialize the key bindings.
+    keys::BoundKeys::get(&config.bindings);
 
-  // Export to csv
-  if (not(cli_data.beg_date.empty()) and not(cli_data.end_date.empty())
-      and not(cli_data.export_file.empty())) {
-    exporter::CSVExporter(cli_data.beg_date, cli_data.end_date,
-                          cli_data.export_file);
-    return 0; // Terminate the program after CSV export.
+    // Export to csv
+    if (not(cli_data.beg_date.empty()) and not(cli_data.end_date.empty())
+        and not(cli_data.export_file.empty())) {
+      exporter::CSVExporter(cli_data.beg_date, cli_data.end_date,
+                            cli_data.export_file);
+      return 0; // Terminate the program after CSV export.
+    }
+
+    // initialize the DurationDisplayer
+    time_lib::DurationDisplayer::get(config.hours_per_workday);
+
+    // Loading the UI and entering input loop.
+    tui::UI tui;
+    tui.input_loop();
+
+    log_lib::logger().tock(); // For measuring teardown time.
+  } catch (std::exception &e) {
+    std::cerr << "Exception caught: " << e.what() << std::endl;
+    return 1;
   }
-
-  // initialize the DurationDisplayer
-  time_lib::DurationDisplayer::get(config.hours_per_workday);
-
-  // Loading the UI and entering input loop.
-  tui::UI tui;
-  tui.input_loop();
-
-  log_lib::logger().tock(); // For measuring teardown time.
 }
