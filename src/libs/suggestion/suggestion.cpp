488189ac56aa35d26171db55aa7c4f1033486dbf
Thomas HOULLIER <pro@houllier.net>
2024-11-23 09:03:05 +0100

    Changed the suggestion algorithm to one which prefers submatches.

--- src/libs/suggestion/suggestion.cpp
+++ src/libs/suggestion/suggestion.cpp
@@ -5,7 +5,7 @@ namespace suggestion{
                          const std::vector<std::string> &choices) {
     double best_score {0.0};
     std::string best_match;
-    rapidfuzz::fuzz::CachedPartialTokenSetRatio<std::string::value_type>
+    rapidfuzz::fuzz::CachedTokenSortRatio<std::string::value_type>
       scorer(query);
     for (const auto &choice : choices) {
       double score = scorer.similarity(choice, best_score);
