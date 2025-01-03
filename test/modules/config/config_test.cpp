336ee3496cebab028107c5ea92c6a918cadcf503
Thomas HOULLIER <pro@houllier.net>
2025-01-03 16:54:37 +0100

    Wrote all config module tests except for key bindings.

--- test/modules/config/config_test.cpp
+++ test/modules/config/config_test.cpp
@@ -3,19 +3,20 @@
 #include "config/config.h"
 #include <catch2/catch_test_macros.hpp>
 #include <fstream>
+#include <type_traits>
 
 std::filesystem::path
 generate_example_config(const std::filesystem::path &dirpath) {
   const std::string content =
     "[db]\n"
-    "file = \"~/.local/timesheeting/timesheeting.db\"\n"
+    "file = \"/tmp/timesheeting.db\"\n"
     "\n"
     "[time]\n"
     "timezone = \"Europe/Paris\"\n"
     "hours_per_workday = 7.7\n"
     "\n"
     "[log]\n"
-    "file = \"~/.local/timesheeting/timesheeting.log\"\n"
+    "file = \"~/tmp/timesheeting.log\"\n"
     "active_levels = [ \"debug\", \"error\", \"info\" ]\n"
     "\n"
     "[keys]\n"
@@ -63,4 +64,30 @@ TEST_CASE("MT-CON-010 Config file loading", "[config]") {
 TEST_CASE("MT-CON-020 Config file non-existent", "[config]") {
   std::filesystem::path nonexistent_path = "/dev/null/nonexistent";
   CHECK_THROWS(config::ConfigLoader().load(nonexistent_path));
+}
+
+TEST_CASE("Configuration contents", "[config]") {
+  auto dir = test_utils::TempDir("timesheeting_config_lib_test");
+  auto filepath = generate_example_config(dir.dirpath);
+  auto config = config::ConfigLoader().load(filepath);
+  SECTION("MT-CON-030 Log filepath") {
+    CHECK(std::is_same<decltype(config.log_filepath),
+          std::filesystem::path>());
+    CHECK(not(config.log_filepath.empty()));
+  }
+  SECTION("MT-CON-040 Log levels") {
+    CHECK(std::is_same<decltype(config.log_levels_to_activate),
+                       std::vector<std::string>>());
+  }
+  SECTION("MT-CON-050 DB filepath") {
+    CHECK(std::is_same<decltype(config.db_filepath), std::filesystem::path>());
+    CHECK(not(config.db_filepath.empty()));
+  }
+  SECTION("MT-CON-060 Timezone string") {
+    CHECK(std::is_same<decltype(config.timezone), std::string>());
+    CHECK(not(config.timezone.empty()));
+  }
+  SECTION("MT-CON-070 Duration display days") {
+    CHECK(std::is_same<decltype(config.hours_per_workday), float>());
+  }
 }
