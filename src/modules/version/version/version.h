bc9a37d1808c40028481e1e9cef9ba2446d846e5
Thomas HOULLIER <pro@houllier.net>
2025-02-22 14:44:33 +0100

    v0.5 ready for merge.

--- src/modules/version/version/version.h
+++ src/modules/version/version/version.h
@@ -7,7 +7,7 @@
 
 namespace version {
   /** @brief Version of the program. */
-  static constexpr std::string TIMESHEETING_VERSION {"0.4dev"};
+  static constexpr std::string TIMESHEETING_VERSION {"0.5"};
   /** @brief Version of the DB. */
   static constexpr int TIMESHEETING_DB_VERSION {2};
 }
