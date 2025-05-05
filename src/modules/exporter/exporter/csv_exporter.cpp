005e5d24ec1d1ce80aafa987765a9ceb21177561
Thomas HOULLIER <pro@houllier.net>
2025-05-05 15:54:58 +0200

    Refactored the exporter module. Tweaked cppcheck to avoid false positive.

--- src/modules/exporter/exporter/csv_exporter.cpp
+++ src/modules/exporter/exporter/csv_exporter.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <memory>
 #include <stdexcept>
+#include <string>
 #include <generator>
 #include "core/export_row.h"
 
@@ -14,31 +15,38 @@ namespace exporter {
 CSVExporter::CSVExporter(const time_lib::DateRange &date_range,
                          std::generator<core::ExportRow> &rows,
                          const std::filesystem::path &export_filepath) {
-  // Errors
   if (std::filesystem::is_directory(export_filepath))
     throw std::runtime_error("Export path is a directory.");
   if (export_filepath.extension() != ".csv")
     throw std::runtime_error("Export file extension must be '.csv'.");
   if (std::filesystem::exists(export_filepath))
     throw std::runtime_error("Export file already exists.");
-  // Open file
   auto export_file = std::make_unique<std::ofstream>(export_filepath);
   if (!export_file->good()) {
     throw std::runtime_error("Export file was not opened correctly.");
   }
-  // Write header
+  write_header(export_file, date_range);
+  write_body(export_file, rows);
+}
+
+void CSVExporter::write_header(std::unique_ptr<std::ofstream> const &export_file,
+                               const time_lib::DateRange &date_range) {
   *export_file << "# Export date: " << time_lib::Date().to_string() << std::endl
-               << "# Export start date: "
-               << date_range.start.to_string() << std::endl
-               << "# Export stop date: "
-               << date_range.stop.to_string() << std::endl
+               << "# Export start date: " << date_range.start.to_string()
+               << std::endl
+               << "# Export stop date: " << date_range.stop.to_string()
+               << std::endl
                << "# Header timezone: " << time_lib::TimeZone::get().name()
                << std::endl
                << "# timesheeting version: " << version::TIMESHEETING_VERSION
                << std::endl
                << "# timesheeting DB version: "
                << std::to_string(version::TIMESHEETING_DB_VERSION) << std::endl;
-  // Write body
+}
+
+void CSVExporter::write_body(std::unique_ptr<std::ofstream> const &export_file,
+                             // cppcheck-suppress constParameterReference
+                             std::generator<core::ExportRow> &rows) {
   *export_file
       << "Entry ID, Project ID, Project name, Task ID, Task name, Location ID, "
          "Location name, Start date, Stop date"
--- src/modules/exporter/exporter/csv_exporter.h
+++ src/modules/exporter/exporter/csv_exporter.h
@@ -5,8 +5,10 @@
 
 #include "core/export_row.h"
 #include "time_lib/date_range.h"
+#include <fstream>
 #include <generator>
 #include <filesystem>
+#include <memory>
 
 namespace exporter {
   /** @brief Exporter of DB data to a CSV file. */
@@ -16,6 +18,13 @@ namespace exporter {
     explicit CSVExporter(const time_lib::DateRange &date_range,
                          std::generator<core::ExportRow> &rows,
                          const std::filesystem::path &export_filepath);
+  private:
+    /** @brief Write the header part of the export file. */
+    void write_header(std::unique_ptr<std::ofstream> const &export_file,
+                      const time_lib::DateRange &date_range);
+    /** @brief Write the body part of the export file. */
+    void write_body(std::unique_ptr<std::ofstream> const &export_file,
+                    std::generator<core::ExportRow> &rows);
   };
 } // namespace exporter
 
