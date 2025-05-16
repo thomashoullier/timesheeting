77d15c0d4c1bbb913fd00c97bc6edb2d0fee4282
Thomas HOULLIER <pro@houllier.net>
2025-05-16 19:55:27 +0200

    Implemented the exporter module tests.

--- test/modules/exporter/exporter_test.cpp
+++ test/modules/exporter/exporter_test.cpp
@@ -5,11 +5,14 @@
 #include "time_lib/time_zone.h"
 #include "test_utils/test_utils.h"
 #include "exporter/csv_exporter.h"
+#include "version/version.h"
 #include <catch2/catch_test_macros.hpp>
 #include <cstdint>
 #include <filesystem>
 #include <ios>
+#include <regex>
 #include <sstream>
+#include <string>
 #include <vector>
 
 std::generator<core::ExportRow>
@@ -98,25 +101,154 @@ TEST_CASE("Exporter module") {
       std::string file_str = ss.str();
       CHECK(file_str.back() == '\n');
     }
-    SECTION("MT-EXP-090 File structure") {
-      std::ifstream in(exporter_test_filepath,
-                       std::ios_base::in | std::ios_base::binary);
+    SECTION("Lines set") {
       std::vector<std::string> lines;
-      std::string line;
-      while (std::getline(in, line)) {
-        lines.push_back(line);
+      {
+        std::ifstream in(exporter_test_filepath,
+                         std::ios_base::in | std::ios_base::binary);
+        std::string line;
+        while (std::getline(in, line)) {
+          lines.push_back(line);
+        }
       }
-      CHECK(lines.size() == 10);
-      CHECK(lines.at(0).front() == '#');
-      CHECK(lines.at(1).front() == '#');
-      CHECK(lines.at(2).front() == '#');
-      CHECK(lines.at(3).front() == '#');
-      CHECK(lines.at(4).front() == '#');
-      CHECK(lines.at(5).front() == '#');
-      CHECK(lines.at(6).front() != '#');
-      CHECK(lines.at(7).front() != '#');
-      CHECK(lines.at(8).front() != '#');
-      CHECK(lines.at(9).front() != '#');
+      SECTION("MT-EXP-090 File structure") {
+        CHECK(lines.size() == 10);
+        CHECK(lines.at(0).front() == '#');
+        CHECK(lines.at(1).front() == '#');
+        CHECK(lines.at(2).front() == '#');
+        CHECK(lines.at(3).front() == '#');
+        CHECK(lines.at(4).front() == '#');
+        CHECK(lines.at(5).front() == '#');
+        CHECK(lines.at(6).front() != '#');
+        CHECK(lines.at(7).front() != '#');
+        CHECK(lines.at(8).front() != '#');
+        CHECK(lines.at(9).front() != '#');
+      }
+      SECTION("Export date") {
+        std::string reference_prefix {"# Export date: "};
+        std::string line = lines.at(0);
+        SECTION("MT-EXP-100 Export date line") {
+          CHECK(line.size() > reference_prefix.size());
+          CHECK(line.substr(0, reference_prefix.size()) == reference_prefix);
+        }
+        SECTION("MT-EXP-110 Export date value") {
+          time_lib::Date date_now{};
+          std::string date_str =
+              line.substr(reference_prefix.size());
+          time_lib::Date export_date{time_lib::Date::FullString{}, date_str};
+          uint64_t error_tolerance = 60; // seconds
+          CHECK((date_now.to_unix_timestamp() -
+                 export_date.to_unix_timestamp()) < error_tolerance);
+        }
+      }
+      SECTION("MT-EXP-120 Export date timezone") {
+        std::string line = lines.at(0);
+        auto line_size = line.size();
+        CHECK(line_size >= 5);
+        std::string last_five = line.substr(line_size - 5);
+        CHECK(last_five.size() == 5);
+        std::regex re("^[+-][0-9]{4}$");
+        CHECK(std::regex_search(last_five, re));
+      }
+      SECTION("Period start date") {
+        std::string reference_prefix{"# Export start date: "};
+        std::string line = lines.at(1);
+        SECTION("MT-EXP-130 Period start date format") {
+          CHECK(line.size() > reference_prefix.size());
+          CHECK(line.substr(0, reference_prefix.size()) == reference_prefix);
+        }
+        SECTION("MT-EXP-140 Period start date value") {
+          std::string after_prefix = line.substr(reference_prefix.size());
+          std::string reference_str{"01Jan2024 00:00:00 +0100"};
+          CHECK(after_prefix == reference_str);
+        }
+      }
+      SECTION("Period stop date") {
+        std::string reference_prefix{"# Export stop date: "};
+        std::string line = lines.at(2);
+        SECTION("MT-EXP-150 Period stop date format") {
+          CHECK(line.size() > reference_prefix.size());
+          CHECK(line.substr(0, reference_prefix.size()) == reference_prefix);
+        }
+        SECTION("MT-EXP-160 Period stop date value") {
+          std::string after_prefix = line.substr(reference_prefix.size());
+          std::string reference_str{"31Dec2024 23:59:59 +0100"};
+          CHECK(after_prefix == reference_str);
+        }
+      }
+      SECTION("Header timezone") {
+        std::string reference_prefix{"# Header timezone: "};
+        std::string line = lines.at(3);
+        SECTION("MT-EXP-170 Header timezone format") {
+          CHECK(line.size() > reference_prefix.size());
+          CHECK(line.substr(0, reference_prefix.size()) == reference_prefix);
+        }
+        SECTION("MT-EXP-180 Header timezone value") {
+          std::string reference_str{"Europe/Paris"};
+          std::string after_prefix = line.substr(reference_prefix.size());
+          CHECK(after_prefix == reference_str);
+        }
+      }
+      SECTION("Program version") {
+        std::string reference_prefix{"# timesheeting version: "};
+        std::string line = lines.at(4);
+        SECTION("MT-EXP-190 Program version format") {
+          CHECK(line.size() > reference_prefix.size());
+          CHECK(line.substr(0, reference_prefix.size()) == reference_prefix);
+        }
+        SECTION("MT-EXP-200 Program version value") {
+          std::string reference_str{version::TIMESHEETING_VERSION};
+          std::string after_prefix = line.substr(reference_prefix.size());
+          CHECK(after_prefix == reference_str);
+        }
+      }
+      SECTION("DB version") {
+        std::string reference_prefix{"# timesheeting DB version: "};
+        std::string line = lines.at(5);
+        SECTION("MT-EXP-210 DB version format") {
+          CHECK(line.size() > reference_prefix.size());
+          CHECK(line.substr(0, reference_prefix.size()) == reference_prefix);
+        }
+        SECTION("MT-EXP-220 DB version value") {
+          auto reference_version = version::TIMESHEETING_DB_VERSION;
+          std::string reference_str = std::to_string(reference_version);
+          std::string after_prefix = line.substr(reference_prefix.size());
+          CHECK(after_prefix == reference_str);
+        }
+      }
+      SECTION("MT-EXP-230 CSV format") {
+        std::regex re(",\\S");
+        for (int iline = 6; iline < 10; ++iline) {
+          std::string line = lines.at(iline);
+          CHECK(line.find(','));
+          CHECK_FALSE(std::regex_search(line, re));
+        }
+      }
+      SECTION("MT-EXP-240 Column list") {
+        const std::string reference_string{
+            "Entry ID, Project ID, Project name, Task ID, Task name, Location "
+            "ID, Location name, Start date, Stop date"};
+        const std::string line = lines.at(6);
+        CHECK(line == reference_string);
+      }
+      SECTION("MT-EXP-250 No empty fields") {
+        std::regex re(",\\s*,");
+        for (int iline = 6; iline < 10; ++iline) {
+          std::string line = lines.at(iline);
+          CHECK_FALSE(std::regex_search(line, re));
+        }
+      }
+      SECTION("MT-EXP-260 Entries lines") {
+        const std::string entry_1{
+            "4, 1, Project1, 3, Task3, 1, Location1, 1729952454, 1729953654"};
+        CHECK(lines.at(7) == entry_1);
+        const std::string entry_2{
+            "8, 1, Project1, 11, Task11, 1, Location1, 1729953659, 1729953789"};
+        CHECK(lines.at(8) == entry_2);
+        const std::string entry_3{
+            "9, 15, Project15, 5, Task5, 3, Location3, 1729953888, 1729953988"};
+        CHECK(lines.at(9) == entry_3);
+      }
     }
   }
 }
