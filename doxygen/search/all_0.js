0140d8be47e027ecb8b77298eef14aa4da8d52ac
Thomas HOULLIER <pro@houllier.net>
2025-05-25 12:40:40 +0000

    CI github pages deployment.

--- doxygen/search/all_0.js
+++ doxygen/search/all_0.js
@@ -5,7 +5,7 @@ var searchData=
   ['active_5flevels_2',['active_levels',['../classlog__lib_1_1Logger.html#a10cb5bd4605bf46ad6265f16f820a20f',1,'log_lib::Logger']]],
   ['activelevels_3',['ActiveLevels',['../structlog__lib_1_1Logger_1_1ActiveLevels.html',1,'log_lib::Logger']]],
   ['add_5fbinding_4',['add_binding',['../classconfig_1_1BindingMap.html#ab6eb40a29cb3b91935b10bdbd0632894',1,'config::BindingMap']]],
-  ['add_5fchar_5',['add_char',['../classncurses__lib_1_1BarNCurses.html#a5a35939248c06f5cd2c140f1afc635df',1,'ncurses_lib::BarNCurses']]],
+  ['add_5fchar_5',['add_char',['../classncurses__lib_1_1BarNCurses.html#afc744363b3124e791f112b519fa8bb76',1,'ncurses_lib::BarNCurses']]],
   ['add_5fitem_6',['add_item',['../classtui_1_1LocationsScreen.html#abcac0f1e6899560f6aa0f29d88adf045',1,'tui::LocationsScreen::add_item()'],['../classtui_1_1ProjectTaskScreen.html#a35b81f8e17ee4d0fc0b09efa5ad94b45',1,'tui::ProjectTaskScreen::add_item()']]],
   ['add_5flocation_7',['add_location',['../classdb_1_1DB__SQLite.html#a4fb1e0bedcaf47650d07cb4de6396de1',1,'db::DB_SQLite']]],
   ['add_5fproject_8',['add_project',['../classdb_1_1DB__SQLite.html#a2c5fac4820f9ccf1ad45748fe82670a2',1,'db::DB_SQLite']]],
